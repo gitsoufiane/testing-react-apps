abca0fe7d070ee0823573ca4674ee1276acc179e
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2021-12-20 13:49:14 -0800

    check error from api if password missing test

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -6,25 +6,26 @@ import * as React from 'react'
 import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
-
+import {handlers} from 'test/server-handlers'
 import {rest} from 'msw'
 import {setupServer} from 'msw/node'
 import Login from '../../components/login-submission'
 
-const server = setupServer(
-  rest.post(
-    'https://auth-provider.example.com/api/login',
-    async (req, res, ctx) => {
-      if (!req.body.password) {
-        return res(ctx.status(400), ctx.json({message: 'password required'}))
-      }
-      if (!req.body.username) {
-        return res(ctx.status(400), ctx.json({message: 'username required'}))
-      }
-      return res(ctx.json({username: req.body.username}))
-    },
-  ),
-)
+// const server = setupServer(
+//   rest.post(
+//     'https://auth-provider.example.com/api/login',
+//     async (req, res, ctx) => {
+//       if (!req.body.password) {
+//         return res(ctx.status(400), ctx.json({message: 'password required'}))
+//       }
+//       if (!req.body.username) {
+//         return res(ctx.status(400), ctx.json({message: 'username required'}))
+//       }
+//       return res(ctx.json({username: req.body.username}))
+//     },
+//   ),
+// )
+const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
 afterAll(() => server.close())
@@ -46,4 +47,17 @@ test(`logging in displays the user's username`, async () => {
   await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
 
   expect(screen.getByText(username)).toBeInTheDocument()
+})
+
+test('omitting the password results as an error', async () => {
+  render(<Login />)
+  const { username } = buildLoginForm()
+  
+  userEvent.type(screen.getByLabelText(/username/i), username)
+  userEvent.click(screen.getByRole('button', { name: /submit/i }))
+  
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+  
+  expect(screen.getByRole('alert')).toHaveTextContent(/password required/i)
+
 })
